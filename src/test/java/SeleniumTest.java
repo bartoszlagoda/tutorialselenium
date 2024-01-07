412e172012386db1675af5223abe8f207e5f8ab1
bartosz.lagoda <composit095@gmail.com>
2024-01-07 12:36:44 +0100

    SELENIUM-150: Create getDriver method for choosing web browser

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -1,18 +1,22 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.annotations.Test;
 
 public class SeleniumTest {
     @Test
     public void openGooglePage(){
-        // Wskazanie ścieżki do WebDrivera
-        /*
-        String path = "C:\\Users\\barte\\PycharmProjects\\tutorial_selenium\\drivers\\chromedriver.exe";
-        System.setProperty("webdriver.chrome.driver", path);
-        */
         //otwieranie przeglądarki Chrome (potrzebny jest zainstalowany chromedriver zgodny z wersją Chrome)
-        WebDriver driver = new ChromeDriver();
-        // wejście na stronę google.com
-        driver.get("https://www.google.com");
+        WebDriver driver = getDriver("edge");
+        driver.get("https://www.google.com/");
+    }
+
+    public WebDriver getDriver(String browser) throws IllegalArgumentException{
+        if(browser.equals("chrome")){
+            return new ChromeDriver();
+        }else if(browser.equals("edge")){
+            return new EdgeDriver();
+        }
+        throw new IllegalArgumentException("Allowed values are ['chrome','edge']");
     }
 }
