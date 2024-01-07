d41887e6ae11a9afc3326d433662a6678337bb9c
bartosz.lagoda <composit095@gmail.com>
2024-01-07 18:06:25 +0100

    SELENIUM-155: IE settings

--- src/test/java/WebDriverManagerTest.java
+++ src/test/java/WebDriverManagerTest.java
@@ -1,12 +1,34 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.UnexpectedAlertBehaviour;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.ie.InternetExplorerOptions;
 import org.testng.annotations.Test;
 
 public class WebDriverManagerTest {
     @Test
-    public void openBrowser(){
+    public void openIEBrowser(){
+        WebDriverManager.iedriver().setup();
+        InternetExplorerOptions options = new InternetExplorerOptions();
+        options.ignoreZoomSettings();
+        options.introduceFlakinessByIgnoringSecurityDomains();
+        options.withInitialBrowserUrl("https://www.google.com");
+        WebDriver driver = new InternetExplorerDriver(options);
+        driver.manage().window().maximize();
+        driver.quit();
+    }
+    @Test
+    public void openChromeBrowser(){
         WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new"); // wywołanie testu w tle
+        options.setUnhandledPromptBehaviour(UnexpectedAlertBehaviour.ACCEPT); // akceptowanie alertów
+        WebDriver driver = new ChromeDriver(options);
+        JavascriptExecutor executor = (JavascriptExecutor) driver;
+        executor.executeScript("alert('Hello')");
+        driver.get("https://www.google.com");
     }
 }
