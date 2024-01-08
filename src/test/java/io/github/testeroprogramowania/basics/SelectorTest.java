80e18973af3b26b6a12ce8ad9471b3979054a937
bartosz.lagoda <composit095@gmail.com>
2024-01-08 19:46:22 +0100

    SELENIUM-165: Create first test for testeroprogramowania.github.io

---
+++ src/test/java/io/github/testeroprogramowania/basics/SelectorTest.java
@@ -0,0 +1,20 @@
+package io.github.testeroprogramowania.basics;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class SelectorTest {
+    @Test
+    public void findElements(){
+        //zainicjowanie chromedrivera (jeśli webdriver nie jest zainstalowany, manager go zainstaluje za nas)
+        WebDriverManager.chromedriver().setup();
+        // uruchomienie przeglądarki Chrome
+        WebDriver driver = new ChromeDriver();
+        // otworzenie okna przeglądarki na pełnym ekranie
+        driver.manage().window().maximize();
+        // przejście do strony testeroprogramowania
+        driver.get("https://testeroprogramowania.github.io/selenium/basics.html");
+    }
+}
