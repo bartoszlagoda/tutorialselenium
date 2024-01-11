550b16b5a7f772a62e556c5eb33a7fceff68ca2b
bartoszlagoda <composit095@gmail.com>
2024-01-11 08:20:45 +0100

    SELENIUM-181: xpath part IV

--- src/test/java/io/github/testeroprogramowania/basics/XPathSelectorsTest.java
+++ src/test/java/io/github/testeroprogramowania/basics/XPathSelectorsTest.java
@@ -24,18 +24,44 @@ public class XPathSelectorsTest {
         WebElement clickOnMeId = driver.findElement(cssId);
 
         // znajdź element o name=fname
-        By firstName = By.xpath("//input[@name='fname'");
+        By firstName = By.xpath("//input[@name='fname']");
         WebElement firstNameInput = driver.findElement(firstName);
 
         // znajdź element o class=topSecret
         By paraHidden = By.xpath("//p[@class='topSecret']");
         driver.findElement(paraHidden);
 
+        // w CSSSelector nie ma takiej opcji
+        // znajdź link w3schools na stronie po tekście opisującym ten link
+        driver.findElement(By.xpath("//a[text()='Visit W3Schools.com!']"));
+
+        // w CSSSelector nie ma takiej opcji
+        // znajdź link w3schools na stronie po fragmencie tekstu opisującym ten link
+        driver.findElement(By.xpath("//a[contains(text(),'Visit')]"));
+
         driver.findElement(By.xpath("/html/body/table/tbody/tr"));
         // znak podwójnego slash pozwala na pominięcie wszystkich pośrednich ścieżek
         driver.findElement(By.xpath("/html/body/table//tbody/tr"));
         driver.findElement(By.xpath("/html/body/div/ul"));
         driver.findElement(By.xpath("//ul"));
+
+        // Jak korzystać z indeksów w XPath?
+        // lokalizowanie elementu o dowolnym tagu - zwrócenie wszystkich elementów na stronie
+        driver.findElement(By.xpath("//*"));
+        // lokalizowanie pierwszego elementu o dowolnym tagu w strukturze - czyli html
+        driver.findElement(By.xpath("*"));
+        driver.findElement(By.xpath("/*"));
+        // lokalizowanie pierwszego elementu typu input
+        driver.findElement(By.xpath("(//input)[1]"));
+        // lokalizowanie ostatniego elementu typu input (jeśli nie znamy ilości inputów na stronie)
+        // lepsze rozwiązanie niż w selektorach CSS
+        driver.findElement(By.xpath("(//input)[last()]"));
+        // lokalizowanie ostatniego elementu typu button, który ma id (jeśli nie znamy ilości inputów na stronie)
+        driver.findElement(By.xpath("(//button[@id])[last()]"));
+        //lokalizowanie wszystkich elementów o tagu name = fname
+        driver.findElement(By.xpath("(//*[@name='fname']"));
+        //lokalizowanie wszystkich elementów posiadających tag name = fname
+        driver.findElement(By.xpath("(//*[@name]"));
 
 
 
