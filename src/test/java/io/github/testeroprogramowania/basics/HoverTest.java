26868ad208083966493f51716f1e156685360ab5
bartosz.lagoda <composit095@gmail.com>
2024-01-13 13:37:04 +0100

    SELENIUM-211: Simulation of hover on element - Actions lesson completed

---
+++ src/test/java/io/github/testeroprogramowania/basics/HoverTest.java
@@ -0,0 +1,29 @@
+package io.github.testeroprogramowania.basics;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
+
+public class HoverTest {
+    @Test
+    public void uploadFileTest() {
+        //zainicjowanie chromedrivera (jeśli webdriver nie jest zainstalowany, manager go zainstaluje za nas)
+        WebDriverManager.chromedriver().setup();
+        // uruchomienie przeglądarki Chrome
+        WebDriver driver = new ChromeDriver();
+        // otworzenie okna przeglądarki na pełnym ekranie
+        driver.manage().window().maximize();
+        // przejście do strony testeroprogramowania
+        driver.get("https://testeroprogramowania.github.io/selenium/basics.html");
+
+        Actions actions = new Actions(driver);
+
+        // najechanie na element
+        WebElement heading = driver.findElement(By.tagName("h1"));
+        actions.moveToElement(heading).perform();
+    }
+}
