50120f9c1609c5cca8f950e7513e01ceb73d2b8a
bartosz.lagoda <composit095@gmail.com>
2024-01-14 17:15:22 +0100

    SELENIUM-219: Check if image is downloaded properly on the page

---
+++ src/test/java/io/github/testeroprogramowania/basics/selectors/ImageDownloadProperlyTest.java
@@ -0,0 +1,33 @@
+package io.github.testeroprogramowania.basics.selectors;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class ImageDownloadProperlyTest {
+
+    WebDriver driver;
+    @Test
+    public void elementExist(){
+        //zainicjowanie chromedrivera (jeśli webdriver nie jest zainstalowany, manager go zainstaluje za nas)
+        WebDriverManager.chromedriver().setup();
+        // uruchomienie przeglądarki Chrome
+        driver = new ChromeDriver();
+        // otworzenie okna przeglądarki na pełnym ekranie
+        driver.manage().window().maximize();
+        // przejście do strony testeroprogramowania
+        driver.get("https://testeroprogramowania.github.io/selenium/basics.html");
+
+        // sprawdzenie czy obrazek pojawia się na stronie
+        WebElement image = driver.findElement(By.tagName("img"));
+        String height = image.getAttribute("naturalHeight");
+        System.out.println(height);
+
+        Assert.assertEquals(height,"223");
+    }
+}
